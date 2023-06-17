5e6f9a394e0249249a5b30aad02506bdefb8ed82
eduardolizarazu <jelo2021@gmail.com>
2023-06-17 12:05:49 -0400

    Ruta 404

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
+  template: `<div>
+                <img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=826&t=st=1687017870~exp=1687018470~hmac=bdd26e529f17aac00a2db23e68185cbf48bf96402a2f9ea93e23e2e8179a0588">
+            </div>`,
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {

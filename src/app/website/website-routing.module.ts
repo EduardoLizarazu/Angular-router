be0431d4578170a15ee817e2f1cde599debc6102
eduardolizarazu <jelo2021@gmail.com>
2023-06-18 20:46:54 -0400

    Precarga de modulos

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -19,7 +18,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'category/:id', component: CategoryComponent },
+      { path: 'category', loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule) },
       { path: 'product/:id', component: ProductDetailComponent },
       { path: 'my-cart', component: MyCartComponent },
       { path: 'login', component: LoginComponent },
